fe734cc83c068bf258ab0017488824d6d71705af
Brian Pitta <Brian.Pitta@illumio.com>
2018-10-22 09:45:08 -0400

    removed request body since it will always be nil in the response struct

--- workloads.go
+++ workloads.go
@@ -24,6 +24,7 @@ func updateWorkload(labels []illumioapi.Label, workload illumioapi.Workload) {
 	if config.Logging.LogOnly == false {
 		updateWlAPI, err := illumioapi.UpdateWorkload(pce, payload)
 		if config.Logging.verbose == true {
+			log.Printf("DEBUG - Update WL API HTTP Request Made: %s %v \r\n", updateWlAPI.Request.Method, updateWlAPI.Request.URL)
 			log.Printf("DEBUG - Update WL API for %s Response Status Code: %d \r\n", workload.Hostname, updateWlAPI.StatusCode)
 			log.Printf("DEBUG - Update WL API for %s Response Body: %s \r\n", workload.Hostname, updateWlAPI.RespBody)
 		}
